389965a2a6486a045fabb14660ae5c0947e101ae
ppmnb <123702548+ppmnb@users.noreply.github.com>
2023-02-07 14:05:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,41 @@
 from telethon.sync import TelegramClient
+
 from telethon.sessions import StringSession
+
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-session = os.environ.get("TERMUX")
-SESSION = os.environ.get("TERMUX")
-token = os.environ.get("TOKEN")
-sedthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
-bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
+
+APP_ID = input("APP_id : ")
+
+APP_HASH = input("APP_Hash : ")
+
+BOT_USERNAME = input("uesrnembot : ")
+
+session = input("T : ")
+
+SESSION = input("T :")
+
+token = input("BOT TO : ")
+
+APP_ID1 = APP_ID
+
+APP_HASH1 = APP_HASH
+
+BOT_USERNAME1 = BOT_USERNAME
+
+session1 = session
+
+SESSION1 = SESSION
+
+token1 = token
+
+fifthon = TelegramClient(StringSession(session1), APP_ID1, APP_HASH1)
+
+bot = TelegramClient("bot", APP_ID1, APP_HASH1).start(bot_token=token1)
+
 ispay = ['yes']
+
 ispay2 = ['yes']
+
+print(token1)
+
 bot.start()
